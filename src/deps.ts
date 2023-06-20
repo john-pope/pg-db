e420968c21e02c395efa427fd78b70967b3c5b00
John Pope <75339810+john-pope@users.noreply.github.com>
2023-06-20 13:27:30 -0600

    fix paths

--- src/deps.ts
+++ src/deps.ts
@@ -7,5 +7,5 @@ export {
     UpgradeCommand as CliffyUpgradeCommand,
 } from "https://deno.land/x/cliffy@v0.25.7/command/upgrade/mod.ts";
 
-export { default as postgresjs } from "https://deno.land/x/postgresjs@v3.3.5";
+export * as postgresjs from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
 export { join } from "https://deno.land/std@0.192.0/path/mod.ts";
